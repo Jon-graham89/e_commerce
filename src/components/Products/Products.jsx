ea8dfad587e7c16e6a95d60f62a11ac6a199e6ac
Jon-graham89 <63361721+Jon-graham89@users.noreply.github.com>
2021-08-18 14:20:18 -0400

    updated styling
    added styles to clean up navbar and products display

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import Product from './Product'
+import Product from './Product/Product'
+import useStyles from './Styles'
+
 
 const products = [
     {id:1, name:"Shoes", description:"Running Shoes", price:"79.99", image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg?crop=1.00xw:0.701xh;0,0.229xh&resize=1200:*"},
@@ -8,8 +10,10 @@ const products = [
 ]
 
 const Products = () =>{
+    const classes = useStyles()
     return(
-        <main>
+        <main className={classes.content}>
+        <div className={classes.toolbar}/>
         <Grid container justifyContent="center" spacing={4}>
             {products.map(product=>(
                 <Grid item key={product.id} xs={12} md={4} lg={3}>
